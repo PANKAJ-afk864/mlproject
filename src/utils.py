1332782d69d87e57c697013c3685bd2fe4c3d17d
Pankaj Kumar Singh <your-email@example.com>
2025-06-09 06:33:39 +0530

    First commit

--- src/utils.py
+++ src/utils.py
@@ -1,44 +1,62 @@
 import os
+import sys
+
+import numpy as np 
+import pandas as pd
 import dill
-import numpy as np
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
+
+from src.exception import CustomException
 
 def save_object(file_path, obj):
-    """
-    Saves a Python object to a file using dill serialization.
-    """
     try:
         dir_path = os.path.dirname(file_path)
+
         os.makedirs(dir_path, exist_ok=True)
 
-        with open(file_path, 'wb') as file_obj:
-            dill.dump(obj, file_obj)
-    except Exception as e:
-        raise Exception(f"Error saving object: {e}")
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
 
-def load_object(file_path):
-    """
-    Loads a Python object from a dill file.
-    """
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
-        with open(file_path, 'rb') as file_obj:
-            return dill.load(file_obj)
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=param[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            #model.fit(X_train, y_train)  # Train model
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
     except Exception as e:
-        raise Exception(f"Error loading object: {e}")
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
 
-def evaluate_model(X_train, y_train, X_test, y_test, models: dict):
-    """
-    Evaluate multiple models and return their R2 scores.
-    """
-    report = {}
-    for name, model in models.items():
-        model.fit(X_train, y_train)
-        y_train_pred = model.predict(X_train)
-        y_test_pred = model.predict(X_test)
-
-        train_score = r2_score(y_train, y_train_pred)
-        test_score = r2_score(y_test, y_test_pred)
-
-        report[name] = test_score
-
-    return report
+    except Exception as e:
+        raise CustomException(e, sys)
